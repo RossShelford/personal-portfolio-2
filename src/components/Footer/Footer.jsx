17375ffcf45eb1ca957fec22f5f7dde583ccaf62
Ross Shelford <rossob@live.co.uk>
2022-06-20 21:13:50 +0100

    Changed Design, removed Lets Chat & reworking Download CV and email

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,21 +9,30 @@ function Footer() {
 
   return (
     <footer className={styles.footerMainContainer}>
-      <div className={styles.footerMainContent}>
-          <div className={styles.LetsChatContainer}> 
-            <span className={styles.letsChat}>Let's Chat</span>
-            <span className={styles.letsChat}><svg width="100px" height='50px' fill="none" stroke='#030439' viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 8l4 4m0 0l-4 4m4-4H3" /></svg></span>
-          </div>
-          <span className={styles.downloadCV}>Download CV</span>
-          <a href='r.shelford@hotmail.co.uk' className={styles.mailMe}>r.shelford@hotmail.co.uk</a>
+
+      <div className={styles.contentGrid}>
+        <section className={styles.sectionText}>
+          <span>Navigation</span>
+          <span>Home</span>
+          <span>Skills</span>
+          <span>Work</span>
+          <span>Project Managment Methodology</span>
+        </section>
+
+        <section className={styles.sectionText}>
+          <span>Socials</span>
+          <span>Dribble</span>
+          <span>Github</span>
+          <span>Linkedin</span>
+        </section>
       </div>
 
       <div className={styles.bottomBarContainer}>
         <hr className={styles.bottomBarLine}/>
-        <div className={styles.bottomBarContent}>
+        <section className={styles.bottomBarContent}>
           <span className={styles.bottomBarText}>{copyright}</span>
           <a className={styles.bottomBarText} href='./Privacy'>Privacy</a>
-        </div>
+        </section>
       </div>
     </footer>
   )
