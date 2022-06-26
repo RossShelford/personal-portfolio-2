c4fe41cffb29298bb56f0b3dec205796ef46e163
Ross Shelford <rossob@live.co.uk>
2022-06-26 23:22:07 +0100

    Mapped skills images with url + styling

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 import styles from './Skills.module.css'
 import '../../index.css'
 import TextDivider from './TextDivider.jsx'
-// import data from './data.js'
-
+import data from './data'
 
 function Skills() {
 
@@ -12,7 +11,16 @@ function Skills() {
         <h1>Skills</h1>
         <h3 className={styles.paddingBottom50px}>Some skills that I have aquired over the years</h3>
         <TextDivider text={'PROJECT MANAGEMENT'}/>
+        <div className={styles.logoContainer}>
+          {data.map((item) => {
+            return (
+              <a href={item.url} target="_blank" rel="noreferrer">
+              <img className={styles.logoWide} key={item.image} src={item.image} alt='img'/>
+              </a>
+          )})}
+        </div>
         <TextDivider text={'DESIGN'}/>
+        
         <TextDivider text={'DEVELOPMENT'}/>
     </section>
   )
