4494391a52c94cfab6a39d8e5abd948754d84d28
Ross Shelford <rossob@live.co.uk>
2022-07-10 12:56:55 +0100

    Projects Carousel working with temp buttons

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,57 +1,55 @@
-import React from 'react'
-import '../../index.css'
+import React, { useState } from 'react'
 import styles from './Projects.module.css'
-import Imagienear from './Imagineear/LogoMorph.mp4'
-import { useState } from 'react'
-
-const activeStyles = {
-  backgroundColor: 'red',
-  transform: 'translateX(calc(-33% + 24px))',
-}
+import '../../index.css'
+import data from './data.js'
 
 function Projects() {
-  
-  const [activeProject, setActiveProject] = useState()
+
+const [activeIndex, setActiveIndex] = useState(0);
+
+const updateIndex = (newIndex) => {
+  if (newIndex < 0) {
+    newIndex = 0;
+  } else if (newIndex >= 3) {
+    newIndex = 2;
+  }
+
+  setActiveIndex(newIndex);
+}
 
   return (
     
     <section className={styles.projectsMainContainer}>
-        <h1>Projects</h1>
-        <h3 className='paddingBottom50px'>A few selected projects of my own work</h3>
-
-
-        <div className={styles.carouselContainer} 
-          style={activeStyles}>
-          <div id="1" className={styles.childContainer}>
-            <video src={Imagienear} playsInline muted autoPlay="autoplay" loop="loop"/>
-            <h3>Imagineear Rebrand</h3>
-          </div>
-          <div id="2" className={styles.childContainer}>
-            <video src={Imagienear} playsInline muted autoPlay="autoplay" loop="loop"/>
-            <h3>NFT Project</h3>
-          </div>
-          <div id="3" className={styles.childContainer}>
-            <video src={Imagienear} playsInline muted autoPlay="autoplay" loop="loop"/>
-            <h3>Accordion</h3>
-          </div>
-        </div>
-
-<div id="testButtons">
-  <button onClick={()=> console.log({activeProject})}>
-    log
-  </button>
-  <button onClick={()=>setActiveProject(1)}>
-    1
-  </button>
-  <button onClick={()=>setActiveProject(2)}>
-    2
-  </button>
-  <button onClick={()=>setActiveProject(3)}>
-    3
-  </button>
-</div>
-
-
+      <h1>Projects</h1>
+      <h3 className='paddingBottom50px'>A few selected projects of my own work</h3>
+      <div className={styles.carouselContainer}>
+        {data.map((item) => {
+          return (
+            <div 
+            className={styles.carouselItem} 
+            key={item.title + ' item'} 
+            style={{ transform: `translateX(-${(activeIndex * 100)}% )` }}
+            >
+              <video src={item.src} playsInline muted key={item.title + ' video'} autoPlay="autoplay" loop="loop"/>
+              <h3 className={styles.carouselText} key={item.title + ' title'}>{item.title}</h3>
+            </div>
+        )})}
+      </div>
+      
+      <button
+        onClick={()=>{
+          updateIndex(activeIndex + 1)
+        }}
+      >
+        Next
+      </button>
+      <button
+        onClick={()=>{
+          updateIndex(activeIndex - 1)
+        }}
+      >
+        Prev
+      </button>
     </section>
   )
 }
---
+++ src/components/Projects/data.js
@@ -0,0 +1,20 @@
+import Imagineear from './Imagineear/LogoMorph.mp4'
+
+
+const data = [
+    {
+        src: Imagineear,
+        title: 'Imagineear Rebrand',
+    },
+    {
+        src: Imagineear,
+        title: 'NFT Project',
+    },
+    {
+        src: Imagineear,
+        title: 'Accordion',
+    },
+
+]
+
+export default data;
