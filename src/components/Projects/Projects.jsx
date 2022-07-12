8f54077280fe8556c087b2c47885159d6d52a891
Ross Shelford <rossob@live.co.uk>
2022-07-12 20:11:44 +0100

    Added active class opacity

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -18,24 +18,31 @@ const updateIndex = (newIndex) => {
   setActiveIndex(newIndex);
 }
 
+
+
   return (
-    
     <section className={styles.projectsMainContainer}>
       <h1>Projects</h1>
       <h3 className='paddingBottom50px'>A few selected projects of my own work</h3>
+
+      {/* Caroursel */}
       <div className={styles.carouselContainer}>
-        {data.map((item) => {
+        {data.map((item, index) => {
           return (
             <div 
-            className={styles.carouselItem} 
+            className={(activeIndex !== index) ? styles.carouselItemActive : styles.carouselItem} 
             key={item.title + ' item'} 
-            style={{ transform: `translateX(-${(activeIndex * 100)}% )` }}
+            style={{ 
+              transform: `translateX(-${(activeIndex * 100)}% )` 
+            }}
             >
               <video src={item.src} playsInline muted key={item.title + ' video'} autoPlay="autoplay" loop="loop"/>
-              <h3 className={styles.carouselText} key={item.title + ' title'}>{item.title}</h3>
+              <h3 className={styles.carouselText} key={item.title + ' title'}>{item.title}</h3>      
             </div>
         )})}
       </div>
+
+      {/* Buttons */}
       <div className={styles.btnContainer}>
         <Button iconOnly={true} onClick={()=>{ updateIndex(activeIndex - 1) }}>
           <svg width="26px" height="26px" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 19l-7-7m0 0l7-7m-7 7h18" /></svg> 
@@ -44,7 +51,6 @@ const updateIndex = (newIndex) => {
           <svg width="26px" height="26px" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M14 5l7 7m0 0l-7 7m7-7H3" /></svg> 
         </Button>
       </div>
-
     </section>
   )
 }
