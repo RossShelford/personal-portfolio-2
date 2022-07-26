b3ecb22772a2226b49f59346ae3036c6903e3ec3
Ross Shelford <rossob@live.co.uk>
2022-07-26 21:56:13 +0100

    added {'\u00A0'} to prevent orphans on PMMethod

--- src/components/PMMethod/data.js
+++ src/components/PMMethod/data.js
@@ -10,37 +10,37 @@ const data = [
         image: define,
         title: 'Step 1 - Define',
         titleColor: 'rgb(237, 62, 122)',
-        copytext: 'Before the work starts, the whole team should understand the end goal, the value that will be provided, and how it will be achieved.',
+        copytext: 'Before the work starts, there should be a discussion where the whole team should understand the end goal, the value that will be provided, and how it will be\u00A0achieved.',
     },
     {
         image: strategy,
         title: 'Step 2 - Strategy',
         titleColor: 'rgb(240, 126, 60)',
-        copytext: 'Once the concept is outlined, A roadmap or board is created and constantly maintained. This breaks down of the features that will make up the final product and is reactive to situations that may occur.',
+        copytext: `Once the concept is outlined, A roadmap is created and maintained. This identifies the features that will make up the final product and is reactive to situations that may\u00A0occur.`,
     },
     {
         image: build,
         title: 'Step 3 - Build',
         titleColor: 'rgb(162, 122, 250)',
-        copytext: 'UX designers and developers begin work on their first iteration of the project. Daily Stand-up meetings with the team to check on progress or if there are any new forseeable roadblocks.',
+        copytext: 'UX designers and developers begin work on their first iteration of the project. Daily Stand-up meetings with the team to check on progress or if there are any new forseeable\u00A0roadblocks.',
     },
     {
         image: test,
         title: 'Step 4 - Test',
         titleColor: 'rgb(81, 194, 237)',
-        copytext: 'Either myself or a quality assurance team should test functionality, detect bugs, and record wins and losses to incorperate them into the schedule.',
+        copytext: 'Either myself or a quality assurance team should test functionality, detect bugs, and record wins and losses to incorperate them into the\u00A0schedule.',
     },
     {
         image: launch,
         title: 'Step 5 - Launch',
         titleColor: 'rgb(125, 120, 250)',
-        copytext: 'After the end of the sprint, have a review with the project stakeholders to show them the finished product. With another meeting internally whether the task load was too heavy or too light for each member.',
+        copytext: 'After the end of the sprint, review with the project stakeholders to show them the product. With another meeting internally if the task load was too heavy or too light for each\u00A0member.',
     },
     {
         image: idea,
         title: 'Conclusion',
         titleColor: 'rgb(105, 176, 250)',
-        copytext: 'To me, a project manager is not there to enforce a rigid structure but instead should shape themselves around the team and unite everyone towards a common goal.',
+        copytext: 'To me, a project manager is not there to enforce a rigid structure but instead should shape themselves around the team and unite everyone towards a common\u00A0goal.',
     },
 ]
 
