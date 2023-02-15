f03a04c9859f49ea7e2174a46369d832786c0b4d
Ross Shelford <rossob@live.co.uk>
2023-02-15 23:40:36 +0000

    Updated NFT Project

--- src/components/Projects/GeneralComponents/CaseStudyHeader.jsx
+++ src/components/Projects/GeneralComponents/CaseStudyHeader.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import styles from './CaseStudyHeader.module.css'
 import Button from '../../shared/Button'
 
-function CaseStudyHeader( {body, title, buttonText, badgeArray} ) {
+function CaseStudyHeader( {body, title, buttonText, badgeArray, showButton} ) {
   return (
     <div className={styles.textContainer}>
         <h2> {title} </h2>
@@ -20,12 +20,15 @@ function CaseStudyHeader( {body, title, buttonText, badgeArray} ) {
             ))} 
         </div>
 
+        {showButton ? 
+
         <a href='https://www.imagineear.com' rel="noreferrer" target='_blank'>
             <Button >
                 {buttonText}
                 <svg width="20px" height="20px" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" xmlns="http://www.w3.org/2000/svg"><path strokeWidth={2} d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25" /></svg>
             </Button>
         </a>
+        : null}
 </div>
 <hr className={styles.dividerLine}/>
     </div>
--- src/components/Projects/NFT/PageNFT.jsx
+++ src/components/Projects/NFT/PageNFT.jsx
@@ -6,6 +6,25 @@ import Timelapse from './assets/Timelapse_Bailey.mp4'
 import IPadFrame from '../../shared/IPadFrame'
 import ProjectLoader from '../../LoaderPages/ProjectLoader'
 import {useState, useEffect} from 'react'
+import CaseStudyHeader from '../GeneralComponents/CaseStudyHeader'
+import CaseStudyFooter from '../GeneralComponents/CaseStudyFooter'
+import baileyPic from './assets/BaileyTheCat.png'
+import illustration from './assets/palette.svg'
+import terminal from './assets/terminal.svg'
+
+
+const badgeData = [
+  {
+      icon: illustration,
+      text: 'Illustration',
+      alt: 'Illustration badge'
+  },
+  {
+      icon: terminal,
+      text: 'Development',
+      alt: 'Development badge'
+  },
+]
 
 function PageNFT() {
 
@@ -28,26 +47,49 @@ function PageNFT() {
     <div className={styles.masterContainer}>
         <img className={styles.headerImage} src={NFTslide} alt='header'/>
       <div className={styles.mainContainer}>
+      <CaseStudyHeader 
+              title='I am the sole creator of an NFT' 
+              body='An NFT (Non-Fungible Token) is a type of digital asset that represents a unique, non-interchangeable item stored on a blockchain. NFTs are revolutionary because they can represent anything that can be digitized, including artwork, music, video, game items, and even collectibles. Unlike other digital assets, NFTs are not interchangeable, meaning that each one is unique and cannot be replaced by another. I firmly believe this technology is in its infancy and could be a pivotal tool in the verifying products and licencing.'
+              showButton={false}
+              badgeArray={badgeData}
+            />
         <div className={styles.textContainer}>
-          <h2>What is an NFT?</h2>
+          <h2>Getting started</h2>
           <p>
-            Put simply, NFT's (Non-Fungible Token) are currently most know for digital art that a person who buys an NFT art piece is actually buying the only verifiable version of that piece as minted by the artist. When an artist mints a work, it's at that point that all other copies are just that, copies. The monetary value of that work is attached to the actual NFT connected to the blockchain. NFT's a whole is a technology still in its infancy and I personally believe it will become a pivotal tool in verifying products and licencing.
+            I aimed to understand the blockchain technology to it’s fullest capacity and decided to make my own NFT from start to finish and learn everything there was to know about them. 
           </p>
-          <h2>How Do NFTs Work?</h2>
           <p>
-            NFTs are powered by smart contracts, most commonly on the Ethereum blockchain. NFTs are created by the process of “minting”. This is when a digital file is transformed into a digital asset. The asset is given a unique fingerprint (hash), token name, and token symbol. Once minted, this asset can then be stored on the blockchain, traded, or sold. Like other cryptocurrency transactions, records of ownership and transfers of digital assets will be stored on the blockchain for all to see.
+            Firstly, I needed subject matter and there’s few things in this world that I love more than my cat Bailey and thought to myself if there’s an opportunity for him to be immortalised on a blockchain in some way then I may as well do it.
+          </p>
+        </div>
+        <img src={baileyPic} className={styles.baileyImg} alt='The best cat in the world'/>
+        <div className={styles.textContainer}>
+          <h2>Creating the illustration</h2>
+          <p>
+            As a base for the NFT I made a sketch then painted out a initial concept on my iPad using software called Procreate which is an intuitive digital illustration app which allows you to draw, paint, and design with ease. 
           </p>
         </div>
         <IPadFrame content={Timelapse}/>
-        <p className={styles.textContainer}>
-          The first necessary part of my Project was to have illustration to use, so I drew my cat Bailey. From there to be able to I then drew a bunch of different variants for the eyes, mouth, body and accessories allowing me to layer and generate thousands of unique artworks using open source software by <a className={styles.clickable} target="_Blank" rel="noreferrer" href="https://github.com/hashlips">Hashlips</a>.
-        </p>
+        <div className={styles.textContainer}>
+          <h2>Creating variety</h2>
+          <p>
+          To create thousands of NFT’s you must have many unique variations. To achieve this, I made a bunch of different features and items, which I layered onto the original artwork and configured an open-source script by <a className={styles.clickable} target="_Blank" rel="noreferrer" href="https://github.com/hashlips">Hashlips</a> that generates random and unique images from layers.
+          </p>
+        </div>
         <video playsInline className={styles.ac} src={NFTGenVid} loop="loop" autoplay="autoplay"></video>
-        <p className={styles.textContainer}>
-          NFTs do not actually store images on the blockchain because of file size constraints. Instead, they store a hash of the image. This hash is called the NFT’s content ID (CID)  and is typically hosted on <a className="underline text-lavender" target="_Blank" rel="noreferrer" href="https://ipfs.io/">IPFS</a>. Once content is uploaded, it cannot be modified without changing the CID.
-          I have used <a className={styles.clickable} target="_Blank" rel="noreferrer" href="https://www.pinata.cloud/">Pinata</a> to simplify   the process of uploading my artwork on IPFS.
-        </p>
+        <div className={styles.textContainer}>
+          <h2>Adding to blockchain</h2>
+          <p>
+            NFTs do not actually store images on the blockchain because of file size constraints. Instead, they store a hash of the image. This hash is called the NFT’s content ID (CID)  and is typically hosted on <a className={styles.clickable} target="_Blank" rel="noreferrer" href="https://ipfs.tech/">IPFS</a>. Once content is uploaded, it cannot be modified without changing the CID.
+            I have used <a className={styles.clickable} target="_Blank" rel="noreferrer" href="https://www.pinata.cloud/">Pinata</a> to simplify the process of uploading my artwork on IPFS.
+          </p>
       </div>
+      </div>
+      <div className={styles.footerWrapper}>
+          <div className={styles.footerContent}>
+            <CaseStudyFooter/>
+          </div>
+        </div>
       {topScroll()}
     </div>
     }
